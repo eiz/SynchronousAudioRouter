7a97f5745408dad849fb03f78a1975b15c7bc97a
Mackenzie Straight <eizneckam@gmail.com>
2015-08-16 14:22:02 -0700

    Start implementing ioctl handler

--- SarCtl/SarCtl.cpp
+++ SarCtl/SarCtl.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <windows.h>
 
+#include "sar.h"
+
+BOOL createEndpoint(
+    HANDLE device, DWORD endpointType, DWORD index, DWORD channelCount,
+    LPWSTR name)
+{
+    SarCreateEndpointRequest request = {};
+
+    request.type = endpointType;
+    request.index = index;
+    request.channelCount = channelCount;
+    wcscpy_s(request.name, name);
+
+    if (!DeviceIoControl(device, SAR_REQUEST_CREATE_ENDPOINT,
+            (LPVOID)&request, sizeof(request), nullptr, 0, nullptr, nullptr)) {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
 int main(int argc, char *argv[])
 {
 	HANDLE device;
-    char buf[1024];
 
     device = CreateFileA("\\\\.\\SynchronousAudioRouter",
         GENERIC_ALL, 0, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
@@ -17,6 +37,12 @@ int main(int argc, char *argv[])
 
     std::cout << "Opened SAR device." << std::endl;
 
-    DeviceIoControl(device, 1, buf, sizeof(buf), nullptr, 0, nullptr, nullptr);
+    if (!createEndpoint(
+        device, SAR_ENDPOINT_TYPE_PLAYBACK, 0, 2, L"Music Out (Stereo)")) {
+        std::cerr << "Couldn't create endpoint. Error "
+            << std::hex << GetLastError() << std::dec << std::endl;
+        return 1;
+    }
+
     return 0;
 }
--- SynchronousAudioRouter/init.cpp
+++ SynchronousAudioRouter/init.cpp
@@ -1,4 +1,4 @@
-#include "sar.hpp"
+#include "sar.h"
 
 extern "C" DRIVER_INITIALIZE DriverEntry;
 
@@ -9,6 +9,9 @@ DRIVER_DISPATCH SarIrpClose;
 
 DRIVER_UNLOAD SarUnload;
 
+#define SAR_LOG(...) \
+    DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_WARNING_LEVEL, __VA_ARGS__)
+
 #define DEVICE_NT_NAME L"\\Device\\SynchronousAudioRouter"
 #define DEVICE_WIN32_NAME L"\\DosDevices\\SynchronousAudioRouter"
 
@@ -18,8 +21,7 @@ NTSTATUS SarIrpUnsupported(PDEVICE_OBJECT deviceObject, PIRP irp)
     PIO_STACK_LOCATION irpStack;
 
     irpStack = IoGetCurrentIrpStackLocation(irp);
-    DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_WARNING_LEVEL,
-        "(SAR) IRP %d not implemented", irpStack->MajorFunction);
+    SAR_LOG("(SAR) IRP %d not implemented", irpStack->MajorFunction);
     irp->IoStatus.Status = STATUS_NOT_IMPLEMENTED;
     IoCompleteRequest(irp, IO_NO_INCREMENT);
     return STATUS_NOT_IMPLEMENTED;
@@ -29,30 +31,91 @@ NTSTATUS SarIrpCreate(PDEVICE_OBJECT deviceObject, PIRP irp)
 {
     UNREFERENCED_PARAMETER(deviceObject);
 
-    DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_WARNING_LEVEL,
-        "(SAR) Device opened");
+    SAR_LOG("(SAR) Device opened");
     irp->IoStatus.Status = STATUS_SUCCESS;
     IoCompleteRequest(irp, IO_NO_INCREMENT);
     return STATUS_SUCCESS;
 }
 
+static BOOL checkIoctlInput(
+    NTSTATUS *status, PIO_STACK_LOCATION irpStack, ULONG size)
+{
+    ULONG length = irpStack->Parameters.DeviceIoControl.InputBufferLength;
+
+    if (length < size) {
+        if (status) {
+            *status = STATUS_BUFFER_TOO_SMALL;
+        }
+
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+// on driver entry: create ks device object
+// on create endpoint: create ks filter & ks pin
+//  - audio category
+//    - for capture endpoints use KSCATEGORY_CAPTURE
+//    - for playback endpoints use KSCATEGORY_RENDER
+//  - filter has 1 pin
+//  - pin has possibly multichannel data format (KSDATARANGE)
+//  - pin supports a single KSDATARANGE
+//  - pin flags: KSPIN_FLAG_DO_NOT_INITIATE_PROCESSING KSPIN_FLAG_PROCESS_IN_RUN_STATE_ONLY KSPIN_FLAG_FIXED_FORMAT KSPIN_FLAG_FRAMES_NOT_REQUIRED_FOR_PROCESSING
+
+static NTSTATUS createEndpoint(SarCreateEndpointRequest *request)
+{
+    UNREFERENCED_PARAMETER(request);
+    return STATUS_NOT_IMPLEMENTED;
+}
+
 NTSTATUS SarIrpDeviceControl(PDEVICE_OBJECT deviceObject, PIRP irp)
 {
     UNREFERENCED_PARAMETER(deviceObject);
+    PIO_STACK_LOCATION irpStack;
+    ULONG ioControlCode;
+    NTSTATUS ntStatus = STATUS_NOT_IMPLEMENTED;
 
-    DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_WARNING_LEVEL,
-        "(SAR) DeviceIoControl IRP not implemented");
-    irp->IoStatus.Status = STATUS_NOT_IMPLEMENTED;
+    irpStack = IoGetCurrentIrpStackLocation(irp);
+    ioControlCode = irpStack->Parameters.DeviceIoControl.IoControlCode;
+
+    SAR_LOG("(SAR) DeviceIoControl IRP: %d", ioControlCode);
+
+    switch (ioControlCode) {
+        case SAR_REQUEST_CREATE_AUDIO_BUFFERS:
+        case SAR_REQUEST_CREATE_ENDPOINT: {
+            SAR_LOG("(SAR) Create endpoint");
+
+            if (!checkIoctlInput(
+                &ntStatus, irpStack, sizeof(SarCreateEndpointRequest))) {
+                break;
+            }
+
+            SarCreateEndpointRequest *request =
+                (SarCreateEndpointRequest *)irp->AssociatedIrp.SystemBuffer;
+
+            SAR_LOG("(SAR) Create endpoint request: %d, %d, %d",
+                request->type, request->index, request->channelCount);
+            ntStatus = createEndpoint(request);
+            break;
+        }
+        case SAR_REQUEST_MAP_AUDIO_BUFFER:
+        case SAR_REQUEST_AUDIO_TICK:
+        default:
+            SAR_LOG("(SAR) Unknown ioctl %d");
+            break;
+    }
+
+    irp->IoStatus.Status = ntStatus;
     IoCompleteRequest(irp, IO_NO_INCREMENT);
-    return STATUS_NOT_IMPLEMENTED;
+    return ntStatus;
 }
 
 NTSTATUS SarIrpClose(PDEVICE_OBJECT deviceObject, PIRP irp)
 {
     UNREFERENCED_PARAMETER(deviceObject);
 
-    DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_WARNING_LEVEL,
-        "(SAR) Device closed");
+    SAR_LOG("(SAR) Device closed");
     irp->IoStatus.Status = STATUS_SUCCESS;
     IoCompleteRequest(irp, IO_NO_INCREMENT);
     return STATUS_SUCCESS;
@@ -62,8 +125,7 @@ VOID SarUnload(PDRIVER_OBJECT driverObject)
 {
     UNICODE_STRING win32DeviceName;
 
-    DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_WARNING_LEVEL,
-        "SAR is unloading");
+    SAR_LOG("SAR is unloading");
     RtlInitUnicodeString(&win32DeviceName, DEVICE_WIN32_NAME);
     IoDeleteSymbolicLink(&win32DeviceName);
     IoDeleteDevice(driverObject->DeviceObject);
@@ -79,7 +141,7 @@ extern "C" NTSTATUS DriverEntry(
     UNICODE_STRING ntDeviceName, win32DeviceName;
     PDEVICE_OBJECT deviceObject;
 
-    DbgPrintEx(DPFLTR_DEFAULT_ID, DPFLTR_WARNING_LEVEL, "Hello, Kernel 3!");
+    SAR_LOG("Hello, Kernel 3!");
     RtlInitUnicodeString(&ntDeviceName, DEVICE_NT_NAME);
     RtlInitUnicodeString(&win32DeviceName, DEVICE_WIN32_NAME);
     status = IoCreateDevice(
